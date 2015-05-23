da68e4840d1ff9a1127890ce3b199a21791cfa6f
eganpg <eganpg@gmail.com>
2015-05-23 12:45:51 -0700

    initial structure

--- client/agstandard.js
+++ client/agstandard.js
@@ -9,14 +9,18 @@ if (Meteor.isClient) {
       
       moltin.Authenticate();
       var product = moltin.Product.List({status: 1});
+      console.log(product)
         return product;
     },
     cartSize: function(){
       var moltin = new Moltin({publicId: 'PJhJh8SAkrZD27vQmpSXyDVC5VpBVfbhG486apa2'});    
-      
       moltin.Authenticate();
-      console.log(moltin);
-      return moltin.Cart.Contents();
+      var contents =  moltin.Cart.Contents();
+      console.log(contents)
+      
+      var arr = Object.keys(contents.contents).map(function(key){return contents.contents[key]});
+      console.log(arr);
+      return arr;
 
         
 
@@ -32,7 +36,7 @@ if (Meteor.isClient) {
       moltin.Authenticate();
       moltin.Cart.Insert(this.id, '1', null, function(cart) {
           console.log(cart);
-          location.reload();
+          // location.reload();
       }, function(error) {
           // Something went wrong...
       });
@@ -46,6 +50,20 @@ if (Meteor.isClient) {
 
        moltin.Cart.Delete();
        location.reload();
+
+      
+    },
+    'click .remove': function(){
+      var moltin = new Moltin({publicId: 'PJhJh8SAkrZD27vQmpSXyDVC5VpBVfbhG486apa2'});    
+      
+      moltin.Authenticate();
+      console.log(this);
+      moltin.Cart.Remove(this.id, function() {
+         console.log('great')
+      }, function(error) {
+          console.log(error);
+      });
+       // location.reload();
 
       
     },
